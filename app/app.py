a715a629e615a1e67c202e3a6eb5f69bf9a516bc
David <20326645+xorye@users.noreply.github.com>
2018-10-27 17:02:56 -0400

    Add userPages.css

--- app/app.py
+++ app/app.py
@@ -3,7 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('signup.html', my_string="Wheeeee!")
+    return render_template('ceo.html', my_string="Wheeeee!")
+
+@app.after_request
+def add_header(r):
+    """
+    This code disable caching in Flask.
+    This behaviour is convenient for development because when refreshing the webpage,
+    cached versions of CSS and JS files are loaded instead of the most recent versions.
+
+    Code from: https://stackoverflow.com/questions/47376744/how-to-prevent-cached-response-flask-server-using-chrome
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 if __name__ == '__main__':
     app.run(debug=True)
