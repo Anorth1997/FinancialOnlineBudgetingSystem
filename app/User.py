cf0f21bc62e57127c06703c57997044071d4a6cc
Ziyao. Chen <ziyao.chen@Chenziyao.local>
2018-10-27 15:24:31 -0400

    dummy start of backend classes

---
+++ app/User.py
@@ -0,0 +1,13 @@
+class User:
+
+    def __init__(self, username, password, company):
+        self.username = username
+        self.password = password
+        self.company = company
+
+
+class CEO(User):
+    def create_department(self, dpt_username, dpt_password):
+
+
+class
