216c95f65ba1dd4feb50c61ce21e65e099d11af8
zhengz55 <43350334+zhengz55@users.noreply.github.com>
2018-10-27 15:54:00 -0400

    Update User.py

--- app/User.py
+++ app/User.py
@@ -7,7 +7,27 @@ class User:
 
 
 class CEO(User):
+    
+  
+    def create_financial_department(self, fd_username, fd_password):
+        
     def create_department(self, dpt_username, dpt_password):
 
 
-class
+class Financial_Department(User):
+    pass
+
+class Department_head(User):
+    def __init__(self, username, password, company, ceo):
+        super().__init__(username, password, company)
+        self.ceo = ceo
+    def submit_request(amount, text):
+        #create a request here
+        request = []
+        if !(amount > self.company.threshold):
+            self.company.Financial_Department.requests.append(request)
+        else:
+            self.company.CEO.dep_req.append(request)
+        return 0
+        
+        
