93116794f3e0dc38c601c958feba90eff1a39cdd
Amney El Azarifi <amneyazarifi@gmail.com>
2018-10-29 15:54:00 -0400

    Added basic getters and setters for the company class

--- app/Company.py
+++ app/Company.py
@@ -3,7 +3,34 @@ class Company:
     def __init__(self, company_name, CEO):
         self.company_name = company_name
         self.CEO = CEO
-        self.FD = None
-        self.Dpts = []  ## list of departments
+        self.fdHead = None
+        self.departments = []  ## list of departments
         self.budget = 0
         self.revenue = 0
+    
+    def get_name(self):
+        return self.company_name
+    
+    def get_FD(self):
+        return self.fdHead
+    
+    def set_FD(self, financialDepartmentHead):
+        self.fdHead = financialDepartmentHead
+    
+    def get_departments(self):
+        return self.departments
+    
+    def set_departments(self, dpt_list):
+        self.departments = dpt_list.copy()
+    
+    def get_budget(self):
+        return self.budget
+    
+    def set_budget(self, newBudget):
+        self.budget = newBudget
+    
+    def get_revenue(self):
+        return self.revenue
+    
+    def set_revenue(self, newRevenue):
+        self.revenue = newRevenue
