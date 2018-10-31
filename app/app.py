9c31ffc8f34cfa11adf7b249b9e296749669bf86
Ziyao. Chen <chenziyao1997@hotmail.com>
2018-10-30 23:20:47 -0400

    dummy database framwork

--- app/app.py
+++ app/app.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+
+
+
 app = Flask(__name__)
+
+#config MySQL
+app.config['']
+app.config['']
+app.config['']
+app.config['']
+app.config['']
+
+#init MYSQL
+mysql = MySQL(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    # form = RegisterForm(request.form)
+    # if request.method == 'POST' and form.validate():
+    #
+    # return render_template('s', form=form)
+
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
@@ -37,6 +59,15 @@ def financial():
 @app.route("/employee")
 def employee():
     return render_template('employee.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired()
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
 if __name__ == '__main__':
     app.run(debug=True)
