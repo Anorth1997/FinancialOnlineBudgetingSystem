8af9cf780267f73e8e5b485f4660c77287934d4d
David <20326645+xorye@users.noreply.github.com>
2018-11-01 13:30:30 -0400

    Fix bug where fade in and fade out was not working

--- app/static/js/userPages.js
+++ app/static/js/userPages.js
@@ -1,12 +1,9 @@
-console.log("Welcome!")
-
 function createDptClicked() {
     hideInitialContent(showCreateDepartmentContent);
 }
 function hideInitialContent(callback) {
-    ('.content-initial').fadeOut(callback);
+    $('.content-initial').fadeOut(callback);
 }
-
 function showCreateDepartmentContent(callback) {
-    ('.content-create-department').fadeIn(callback);
+    $('.content-create-department').fadeIn(callback);
 }
