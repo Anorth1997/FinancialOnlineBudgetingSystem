c876e06e9fc06f6415d2eebd6631af9514361de6
zhengz55 <43350334+zhengz55@users.noreply.github.com>
2018-11-02 20:31:14 -0400

    Update userPages.js

--- app/static/js/userPages.js
+++ app/static/js/userPages.js
@@ -7,3 +7,27 @@ function hideInitialContent(callback) {
 function showCreateDepartmentContent(callback) {
     $('.content-create-department').fadeIn(callback);
 }
+
+function setTotalRevenueClicked() {
+    hideInitialContent(showSetTotalRevenue);
+}
+
+function showSetTotalRevenue(callback) {
+    $('.content-set-total-revenue').fadeIn(callback);
+}
+
+function ReviewFDRequestClicked() {
+    hideInitialContent(showReviewFDRequest);
+}
+
+function showReviewFDRequestCClicked(callback) {
+    %('.content-review-financial-request').fadeIn(callback);
+}
+
+function ViewAllDepartmentHistoryClicked() {
+    hideInitialContent(showViewAllDeptHistory);
+}
+
+function showViewAllDeptHistory(callback) {
+    %('.content-view-department-history').fadeIn(callback);
+}
