72fa456ab510bfd68ee349b46645ab3e55671ab1
David <20326645+xorye@users.noreply.github.com>
2018-11-29 02:22:24 -0500

    Implement View All Department History display in CEO

--- app/static/js/ceo.js
+++ app/static/js/ceo.js
@@ -1,4 +1,35 @@
+function displayAllDepartmentHistory(html) {
+    const initTable = '<table align="left"><tr>' +
+      '<th>Department</th>' +
+      '<th>Purpose</th>' +
+      '<th>Date</th>' +
+      '<th>Amount</th>' +
+      '</tr>'+
+      '</table>';
 
+    let table = $(initTable);
+    const expenses = html.expenses;
+
+    // Keep track of total so that we can display it on the last row
+    let total = 0;
+
+    // For each expense, create a new row in the table
+    for (let i = 0; i < expenses.length; i++) {
+        const currExpense = expenses[i];
+        let row = $('<tr></tr>');
+        $(row).append('<td>' + currExpense.department + '</td>');
+        $(row).append('<td>' + currExpense.purpose + '</td>');
+        $(row).append('<td>' + currExpense.date.slice(0, -4) + '</td>');
+        $(row).append('<td>$' + currExpense.amount + '</td>');
+        $(table).append(row);
+        total += currExpense.amount;
+    }
+
+    // Create the last row that displays the total
+    $(table).append('<tr><td>Total</td><td></td><td></td><td>$' + total.toString() + '</td></tr>');
+
+    $('.department-history-table').append(table);
+}
 
 function getRequests() {
     var requests = data;
@@ -48,6 +79,15 @@ function reviewFDRequestsClicked() {
 function viewAllDepartmentHistoryClicked() {
     $('.graph-button').fadeIn();
     fadeAllCeo(showViewAllDeptHistory);
+
+
+    $.ajax({
+        url: "http://127.0.0.1:5000/expenses/full_history",
+        cache: false,
+        success: function(html){
+            displayAllDepartmentHistory(html);
+        }
+    });
 }
 
 function showCreateDepartmentContent(callback) {
@@ -68,6 +108,8 @@ function showReviewFDRequest(callback) {
 function showViewAllDeptHistory(callback) {
     $('.content-view-department-history').fadeIn(callback);
     fadeInGraphButton();
+
+
 }
 
 var enforeMutualExcludedCheckBox = function(group){
